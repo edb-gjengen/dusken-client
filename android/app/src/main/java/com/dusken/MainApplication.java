da364c48523ccd55ad90acc0409bc7f1e4a2ce3c
Nikolai R Kristiansen <nikolaik@gmail.com>
2017-04-14 02:11:18 +0200

    Bump to RN 0.43

--- android/app/src/main/java/com/dusken/MainApplication.java
+++ android/app/src/main/java/com/dusken/MainApplication.java
@@ -1,7 +1,6 @@
 package com.dusken;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
@@ -18,7 +17,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
