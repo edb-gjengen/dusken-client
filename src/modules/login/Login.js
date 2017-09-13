3bd31e44993fc23c462a993b5a0a8f2471d788ba
Nikolai R Kristiansen <nikolai@lucida.no>
2017-09-13 21:46:24 +0200

    Login: Fix double button press

--- src/modules/login/Login.js
+++ src/modules/login/Login.js
@@ -77,7 +77,7 @@ export default class Login extends Component {
     render() {
         return (
             <Container style={styles.container}>
-                <Content>
+                <Content keyboardShouldPersistTaps='always'>
                     <Form style={styles.card}>
                         {this.emailInput()}
                         {this.passwordInput()}
