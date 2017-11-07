064255fe1ee7a32c9f5563dc6c07a6046460e669
Nikolai R Kristiansen <nikolaik@gmail.com>
2017-11-08 00:33:58 +0100

    Fix tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,7 +4,6 @@ import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import 'isomorphic-fetch';
 
 it('renders correctly', () => {
   const tree = renderer.create(
