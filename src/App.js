5ba57eaacfe236968f0995f845f3c320c5ad1be2
Nikolai R Kristiansen <nikolaik@gmail.com>
2022-07-31 21:36:45 +0200

    ⬆ Upgrade to RN v0.68
    Stripe android lib needs compileSdkVersion = 31
    ref https://github.com/stripe/stripe-react-native/issues/812
    
    To not break anything else in the RN space, we need to bump to RN 0.68

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import Config from 'react-native-config';
-import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {ApolloClient, HttpLink, InMemoryCache, ApolloProvider} from '@apollo/client';
 // import { StripeProvider } from '@stripe/stripe-react-native';
 
 import Dusken from './Dusken';
-import { store, persistor } from './reduxStore';
+import {store, persistor} from './reduxStore';
 
 const apolloClient = new ApolloClient({
-  link: new HttpLink({ uri: `${Config.DUSKEN_API_URL}/api/graphql/` }),
+  link: new HttpLink({uri: `${Config.DUSKEN_API_URL}/api/graphql/`}),
   cache: new InMemoryCache(),
 });
 
-const App = (
-  // <StripeProvider publishableKey={Config.STRIPE_PUBLISHABLE_KEY || ''}>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <ApolloProvider client={apolloClient}>
-        <Dusken />
-      </ApolloProvider>
-    </PersistGate>
-  </Provider>
-  // </StripeProvider>
-);
-
-export default App;
+// <StripeProvider publishableKey={Config.STRIPE_PUBLISHABLE_KEY || ''}>
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ApolloProvider client={apolloClient}>
+          <Dusken />
+        </ApolloProvider>
+      </PersistGate>
+    </Provider>
+  );
+}
+// </StripeProvider>
